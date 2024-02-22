6850074e075663699abcfcec328f2066051c54f8
zioscuro <lore.rang@gmail.com>
2024-02-22 22:51:38 +0100

    Update create_account page url

--- BIM_project_manager/accounts/urls.py
+++ BIM_project_manager/accounts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import create_account_view
 
 urlpatterns = [
-    path('create-account/', create_account_view, name='create_account_view'),    
+    path('create_account/', create_account_view, name='create_account_view'),    
 ]
