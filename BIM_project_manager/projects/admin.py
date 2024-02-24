2763ebb8b64ae5374228807d5818e7a5d0701cdf
zioscuro <lore.rang@gmail.com>
2024-02-24 18:13:24 +0100

    Register projects models in admin app

--- BIM_project_manager/projects/admin.py
+++ BIM_project_manager/projects/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Project, BimModel, InfoSheet, Report, ClashTest, ValidationTest
 
 # Register your models here.
+
+admin.site .register(Project)
+admin.site .register(BimModel)
+admin.site .register(InfoSheet)
+admin.site .register(Report)
+admin.site .register(ClashTest)
+admin.site .register(ValidationTest)
