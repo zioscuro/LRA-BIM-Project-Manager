9c62cea42a1dc22814efe6ae8353bb5dd7b222b5
zioscuro <lore.rang@gmail.com>
2024-02-24 11:59:31 +0100

    Add user_profile page

--- BIM_project_manager/core/urls.py
+++ BIM_project_manager/core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import homepage
+from .views import homepage, user_profile_view
 
 urlpatterns = [
     path('', homepage, name='homepage'),
+    path('user/<str:username>/', user_profile_view, name='user_profile'), 
 ]
--- BIM_project_manager/core/views.py
+++ BIM_project_manager/core/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 
 def homepage(request):
   return render(request, 'core/homepage.html')
+
+def user_profile_view(request, username):
+  user = get_object_or_404(User, username=username)
+  context = {'user': user}
+  return render(request, 'core/user_profile.html', context)
+  
