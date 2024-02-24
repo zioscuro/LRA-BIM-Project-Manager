ba27955d7fbd91cf49fbc2aef11a4cd48666336f
zioscuro <lore.rang@gmail.com>
2024-02-24 12:32:13 +0100

    Add user_list page

--- BIM_project_manager/core/views.py
+++ BIM_project_manager/core/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render
+from django.views.generic.list import ListView
 
 # Create your views here.
 
@@ -10,4 +11,8 @@ def user_profile_view(request, username):
   user = get_object_or_404(User, username=username)
   context = {'user': user}
   return render(request, 'core/user_profile.html', context)
+
+class UserList(ListView):
+  model = User
+  template_name = 'core/users.html'
   
