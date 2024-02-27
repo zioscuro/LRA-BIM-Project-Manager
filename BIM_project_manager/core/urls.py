b46eddf71f22e8d85c50f822c2dd080f08719d17
zioscuro <lore.rang@gmail.com>
2024-02-27 01:29:33 +0100

    Update homepage to view bim projects list

--- BIM_project_manager/core/urls.py
+++ BIM_project_manager/core/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import homepage, user_profile_view, UserList
+from .views import HomeView, user_profile_view, UserList
 
 urlpatterns = [
-    path('', homepage, name='homepage'),
+    path('', HomeView.as_view(), name='homepage'),
     path('users/', UserList.as_view() , name='user_list'),
     path('user/<str:username>/', user_profile_view, name='user_profile'), 
 ]
--- BIM_project_manager/core/views.py
+++ BIM_project_manager/core/views.py
@@ -1,11 +1,17 @@
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render
 from django.views.generic.list import ListView
+from projects.models import BimProject
 
 # Create your views here.
 
-def homepage(request):
-  return render(request, 'core/homepage.html')
+# def homepage(request):
+#   return render(request, 'core/homepage.html')
+
+class HomeView(ListView):
+  queryset = BimProject.objects.all()
+  template_name = 'core/homepage.html'
+  context_object_name = 'BimProjects_list'
 
 def user_profile_view(request, username):
   user = get_object_or_404(User, username=username)
@@ -15,4 +21,6 @@ def user_profile_view(request, username):
 class UserList(ListView):
   model = User
   template_name = 'core/users.html'
+  context_object_name = 'Users_list'
+  
   
