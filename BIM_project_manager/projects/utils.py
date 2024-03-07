d7fb88d3104d0efdf9b8d8a42b1c4b3a3d389a63
zioscuro <lore.rang@gmail.com>
2024-03-07 22:38:24 +0100

    Retrieve and print bim models data in create_model_register utils function

--- BIM_project_manager/projects/utils.py
+++ BIM_project_manager/projects/utils.py
@@ -1,9 +1,16 @@
 from django.http import HttpResponse
 
 def create_model_register(project):
+  bim_models = project.bim_models.all()
+
   response = HttpResponse(content_type='text/plain')  
-  response['Content-Disposition'] = f'attachment; filename="Model_Register_{project.name}.txt"'
-  response.write(f'Registro modelli in utils per il progetto: {project.name}')
+  response['Content-Disposition'] = f'attachment; filename="Model_Register_{project.name}.txt"'  
+  response.write(f'Registro modelli - Progetto: {project.name}\n')
+  response.write(f'n. - Nome - Disciplina - Progettista\n')
+
+  for count, model in enumerate(bim_models):
+    response.write(f'{count+1} - {model.name} - {model.discipline} - {model.designer}\n')
+
   return response
 
 def create_project_info_sheets(project):
