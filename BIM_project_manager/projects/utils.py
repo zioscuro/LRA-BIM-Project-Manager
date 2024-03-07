6db8e1f9917070b098bf9f401aee8d3d87e24829
zioscuro <lore.rang@gmail.com>
2024-03-07 23:21:08 +0100

    Retrieve and print all info sheets reports data for a related model in create_model_info_sheets utils function

--- BIM_project_manager/projects/utils.py
+++ BIM_project_manager/projects/utils.py
@@ -31,8 +31,35 @@ def create_project_info_sheets(project):
   return response
 
 def create_model_info_sheets(model):
+  info_sheets = model.info_sheets.all()
+
   response = HttpResponse(content_type='text/plain')  
   response['Content-Disposition'] = f'attachment; filename="{model.name}_Info_Sheets.txt"'
-  response.write(f'Tutte le schede informative per il modello BIM: {model.name}')
+  response.write(f'Schede informative - Modello: {model.name}')
+
+  for count, sheet in enumerate(info_sheets):
+    response.write(f'Scheda n.{count+1} - {sheet.name} - {sheet.description} - {sheet.sheet_type}\n')
+    
+    reports = sheet.reports.all()
+    
+    for count, report in enumerate(reports):
+      response.write(f'\tReport n.{count+1} - {report.name} - {report.description} - {sheet.sheet_type}\n')
+
+      if sheet.sheet_type == 'coordination':
+        tests = report.clash_tests.all()
+        response.write('\t\tTest Interferenze\n')
+        response.write('\t\tData - Commenti - Nuove - Attive - Revisionate - Approvate - Risolte\n')
+
+        for test in tests:
+          response.write(f'\t\t{test.date.strftime("%m/%d/%Y")} - {test.comments} - {test.clash_new} - {test.clash_active} - {test.clash_reviewed} - {test.clash_approved} - {test.clash_resolved}\n')
+
+      if sheet.sheet_type == 'validation':
+        tests = report.validation_tests.all()
+        response.write('\t\tTest Verifica\n')
+        response.write('\t\tData - Commenti - Specifica - Difformità\n')
+
+        for test in tests:
+          response.write(f'\t\t{test.date.strftime("%m/%d/%Y")} - {test.comments} - {test.specification} - {test.issues}\n')
+
   return response
 
