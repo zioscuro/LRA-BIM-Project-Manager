6930b122f0b456fd8f2ead5910c335977a9fd4f7
zioscuro <lore.rang@gmail.com>
2024-04-07 16:02:30 +0200

    Remove unused file forms.py in projects app

--- BIM_project_manager/projects/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import ClashTest, ValidationTest
-
-
-# class AddValidationTestForm(forms.ModelForm):
-#   class Meta:
-#     model = ValidationTest
-#     fields = ['comments', 'specification', 'issues']
