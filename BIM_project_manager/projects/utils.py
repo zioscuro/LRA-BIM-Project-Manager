293a2015679f38ad88aec7595c5b7a8ec15d3b89
zioscuro <lore.rang@gmail.com>
2024-04-20 15:58:36 +0200

    Add an excel Styles class in utils.py

--- BIM_project_manager/projects/utils.py
+++ BIM_project_manager/projects/utils.py
@@ -3,30 +3,47 @@ from .models import InfoSheet, Report
 from openpyxl import Workbook
 from openpyxl.styles import NamedStyle, Font, Border, Side, Alignment, PatternFill
 
+class Styles():
+  thin_line = Side(border_style="thin", color="000000")
+
+  title = NamedStyle(name="title")
+  title.font = Font(bold=True)
+  title.alignment = Alignment(horizontal='center')
+  title.fill = PatternFill(start_color='C0C0C0', end_color='C0C0C0', fill_type = "solid")
+  title.border = Border(top=thin_line, left=thin_line, right=thin_line, bottom=thin_line)
+
+  header = NamedStyle(name="header")
+  header.font = Font(bold=True)
+  header.border = Border(top=thin_line, left=thin_line, right=thin_line, bottom=thin_line)
+
+  standard_cell = NamedStyle(name="standard cell")
+  standard_cell.border = Border(top=thin_line, left=thin_line, right=thin_line, bottom=thin_line)
+
+  model_header = NamedStyle(name="model header")
+  model_header.font = Font(bold=True)
+  model_header.fill = PatternFill(start_color='ddebf7', end_color='ddebf7', fill_type = "solid")
+  model_header.border = Border(top=thin_line, left=thin_line, right=thin_line, bottom=thin_line) 
+
+  info_header = NamedStyle(name="info header")
+  info_header.font = Font(bold=True)
+  info_header.fill = PatternFill(start_color='ffe699', end_color='ffe699', fill_type = "solid")
+  info_header.border = Border(top=thin_line, left=thin_line, right=thin_line, bottom=thin_line) 
+
+  report_header = NamedStyle(name="report header")
+  report_header.font = Font(bold=True)
+  report_header.fill = PatternFill(start_color='f8cbad', end_color='f8cbad', fill_type = "solid")
+  report_header.border = Border(top=thin_line, left=thin_line, right=thin_line, bottom=thin_line) 
+  report_header.alignment = Alignment(textRotation=90, wrap_text=True, horizontal='center', vertical='center')
+
 def create_model_register_file(bim_project):
   '''
   create an excel file export with the model register with all the BIM model of a specific project
   '''
   bim_models = bim_project.bim_models.all()
 
-  thin_line = Side(border_style="thin", color="000000")
-
-  title_style = NamedStyle(name="title")
-  title_style.font = Font(bold=True)
-  title_style.alignment = Alignment(horizontal='center')
-  title_style.fill = PatternFill(start_color='C0C0C0', end_color='C0C0C0', fill_type = "solid")
-  title_style.border = Border(top=thin_line, left=thin_line, right=thin_line, bottom=thin_line)
-
-  header_style = NamedStyle(name="header")
-  header_style.font = Font(bold=True)
-  header_style.border = Border(top=thin_line, left=thin_line, right=thin_line, bottom=thin_line)
-
-  standard_cell_style = NamedStyle(name="standard cell")
-  standard_cell_style.border = Border(top=thin_line, left=thin_line, right=thin_line, bottom=thin_line)
-
   wb = Workbook()
   wb.remove(wb.active)  
-  ws = wb.create_sheet(title='Model-Register')
+  ws = wb.create_sheet(title='Model-Register')  
 
   ws.column_dimensions['A'].width = 5
   ws.column_dimensions['B'].width = 20
@@ -36,36 +53,36 @@ def create_model_register_file(bim_project):
   ws.column_dimensions['F'].width = 15
 
   ws['A1'] = f'Registro modelli - Progetto: {bim_project.name}'
-  ws['A1'].style = title_style
+  ws['A1'].style = Styles.title
   ws.row_dimensions[1].height = 20
   ws.merge_cells('A1:F1')
 
   ws['A2'] = 'n.'
-  ws['A2'].style = header_style
+  ws['A2'].style = Styles.header
 
   ws['B2'] = 'Nome modello'
-  ws['B2'].style = header_style
+  ws['B2'].style = Styles.header
 
   ws['C2'] = 'Disciplina'
-  ws['C2'].style = header_style
+  ws['C2'].style = Styles.header
 
   ws['D2'] = 'Software'
-  ws['D2'].style = header_style
+  ws['D2'].style = Styles.header
 
   ws['E2'] = 'Scheda LOD'
-  ws['E2'].style = header_style
+  ws['E2'].style = Styles.header
 
   ws['F2'] = 'Progettista'
-  ws['F2'].style = header_style
+  ws['F2'].style = Styles.header
 
   ws.row_dimensions[2].height = 20
 
   for count, bim_model in enumerate(bim_models):
-   ws.append([count+1, bim_model.name, bim_model.discipline, bim_model.authoringSoftware, bim_model.lodReference, bim_model.designer])
-   ws.row_dimensions[count+3].height = 20
+    ws.append([count+1, bim_model.name, bim_model.discipline, bim_model.authoringSoftware, bim_model.lodReference, bim_model.designer])
+    ws.row_dimensions[ws.max_row].height = 20
 
-   for cell in ws[ws.max_row]:
-     cell.style = standard_cell_style
+    for cell in ws[ws.max_row]:
+      cell.style = Styles.standard_cell
 
   response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
   response['Content-Disposition'] = f'attachment; filename="Model_Register_{bim_project.name}.xlsx"'
@@ -79,20 +96,6 @@ def create_project_info_sheets_file(bim_project):
   '''
   bim_models = bim_project.bim_models.all()
 
-  thin_line = Side(border_style="thin", color="000000")
-  
-  title_style = NamedStyle(name="title")
-  title_style.font = Font(bold=True)
-  title_style.alignment = Alignment(horizontal='center')
-  title_style.fill = PatternFill(start_color='C0C0C0', end_color='C0C0C0', fill_type = "solid")
-
-  header_style = NamedStyle(name="header")
-  header_style.font = Font(bold=True)
-  header_style.border = Border(top=thin_line, left=thin_line, right=thin_line, bottom=thin_line)
-
-  standard_cell_style = NamedStyle(name="standard cell")
-  standard_cell_style.border = Border(top=thin_line, left=thin_line, right=thin_line, bottom=thin_line)
-
   wb = Workbook()
   wb.remove(wb.active)
 
@@ -105,21 +108,21 @@ def create_project_info_sheets_file(bim_project):
     ws.column_dimensions['D'].width = 15
     
     ws['A1'] = f'Schede Informative modello: {bim_model.name}'
-    ws['A1'].style = title_style
+    ws['A1'].style = Styles.title
     ws.row_dimensions[1].height = 20
     ws.merge_cells('A1:D1')
 
     ws['A2'] = 'n.'
-    ws['A2'].style = header_style
+    ws['A2'].style = Styles.header
 
     ws['B2'] = 'Nome Scheda'
-    ws['B2'].style = header_style
+    ws['B2'].style = Styles.header
 
     ws['C2'] = 'Descrizione Scheda'
-    ws['C2'].style = header_style
+    ws['C2'].style = Styles.header
 
     ws['D2'] = 'Tipo Scheda'
-    ws['D2'].style = header_style
+    ws['D2'].style = Styles.header
 
     ws.row_dimensions[2].height = 20
 
@@ -129,7 +132,7 @@ def create_project_info_sheets_file(bim_project):
       ws.append([count+1, sheet.name, sheet.description, sheet.sheet_type])
 
       for cell in ws[ws.max_row]: 
-        cell.style = standard_cell_style
+        cell.style = Styles.standard_cell
 
   response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')  
   response['Content-Disposition'] = f'attachment; filename="Project_Info_Sheets_{bim_project.name}.xlsx"'
@@ -142,27 +145,6 @@ def create_model_info_sheets_file(bim_model):
   create an excel file export with all the info sheets of a specific BIM model
   '''
   info_sheets = bim_model.info_sheets.all()
-
-  thin = Side(border_style="thin", color="000000")
-
-  model_header_style = NamedStyle(name="model header")
-  model_header_style.font = Font(bold=True)
-  model_header_style.fill = PatternFill(start_color='ddebf7', end_color='ddebf7', fill_type = "solid")
-  model_header_style.border = Border(top=thin, left=thin, right=thin, bottom=thin) 
-
-  info_header_style = NamedStyle(name="info header")
-  info_header_style.font = Font(bold=True)
-  info_header_style.fill = PatternFill(start_color='ffe699', end_color='ffe699', fill_type = "solid")
-  info_header_style.border = Border(top=thin, left=thin, right=thin, bottom=thin) 
-
-  report_header_style = NamedStyle(name="report header")
-  report_header_style.font = Font(bold=True)
-  report_header_style.fill = PatternFill(start_color='f8cbad', end_color='f8cbad', fill_type = "solid")
-  report_header_style.border = Border(top=thin, left=thin, right=thin, bottom=thin) 
-  report_header_style.alignment = Alignment(textRotation=90, wrap_text=True, horizontal='center', vertical='center')
-
-  standard_cell_style = NamedStyle(name="standard cell")
-  standard_cell_style.border = Border(top=thin, left=thin, right=thin, bottom=thin)
 
   wb = Workbook()
   wb.remove(wb.active)
@@ -180,54 +162,54 @@ def create_model_info_sheets_file(bim_model):
     ws.column_dimensions['H'].width = 15
 
     ws['A1'] = 'DATI MODELLO'
-    ws['A1'].style = model_header_style
+    ws['A1'].style = Styles.model_header
     ws['A1'].alignment = Alignment(textRotation=90, wrap_text=True, horizontal='center', vertical='center')
     ws.merge_cells('A1:A5')
 
     ws['B1'] = 'Nome Modello'
-    ws['B1'].style = model_header_style
+    ws['B1'].style = Styles.model_header
     ws['C1'] = bim_model.name
-    ws['C1'].style = standard_cell_style
+    ws['C1'].style = Styles.standard_cell
 
     ws['B2'] = 'Disciplina'
-    ws['B2'].style = model_header_style
+    ws['B2'].style = Styles.model_header
     ws['C2'] = bim_model.discipline
-    ws['C2'].style = standard_cell_style
+    ws['C2'].style = Styles.standard_cell
 
     ws['B3'] = 'Progettista'
-    ws['B3'].style = model_header_style
+    ws['B3'].style = Styles.model_header
     ws['C3'] = bim_model.designer
-    ws['C3'].style = standard_cell_style
+    ws['C3'].style = Styles.standard_cell
 
     ws['B4'] = 'Software BIM Authoring'
-    ws['B4'].style = model_header_style
+    ws['B4'].style = Styles.model_header
     ws['C4'] = bim_model.authoringSoftware
-    ws['C4'].style = standard_cell_style
+    ws['C4'].style = Styles.standard_cell
 
     ws['B5'] = 'LOD di riferimento'
-    ws['B5'].style = model_header_style
+    ws['B5'].style = Styles.model_header
     ws['C5'] = bim_model.lodReference
-    ws['C5'].style = standard_cell_style    
+    ws['C5'].style = Styles.standard_cell   
 
     ws['A6'] = 'DATI SCHEDA'
-    ws['A6'].style = info_header_style
+    ws['A6'].style = Styles.info_header
     ws['A6'].alignment = Alignment(textRotation=90, wrap_text=True, horizontal='center', vertical='center')
     ws.merge_cells('A6:A8')
 
     ws['B6'] = 'Nome Scheda Informativa'
-    ws['B6'].style = info_header_style
+    ws['B6'].style = Styles.info_header
     ws['C6'] = sheet.name
-    ws['C6'].style = standard_cell_style
+    ws['C6'].style = Styles.standard_cell
 
     ws['B7'] = 'Descrizione Scheda'
-    ws['B7'].style = info_header_style
+    ws['B7'].style = Styles.info_header
     ws['C7'] = sheet.description
-    ws['C7'].style = standard_cell_style
+    ws['C7'].style = Styles.standard_cell
 
     ws['B8'] = 'Tipo Scheda'
-    ws['B8'].style = info_header_style
+    ws['B8'].style = Styles.info_header
     ws['C8'] = sheet.sheet_type
-    ws['C8'].style = standard_cell_style
+    ws['C8'].style = Styles.standard_cell
     
     ws.append([])
     
@@ -236,19 +218,19 @@ def create_model_info_sheets_file(bim_model):
       if sheet.sheet_type == 'coordination':
         ws.append(['', 'Nome Report', report.name])
         for cell in ws[ws.max_row]:
-          cell.style = standard_cell_style
+          cell.style = Styles.standard_cell
           cell.font = Font(bold=True)
         ws.merge_cells(f'C{ws.max_row}:H{ws.max_row}')         
 
         ws.append(['', 'Oggetto/Specifica Report', report.description])
         for cell in ws[ws.max_row]:
-          cell.style = standard_cell_style
+          cell.style = Styles.standard_cell
         ws.merge_cells(f'C{ws.max_row}:H{ws.max_row}')
         
         ws.append(['', 'Data', 'Commenti', 'Nuove', 'Attive', 'Revisionate', 'Approvate', 'Risolte'])
 
         for cell in ws[ws.max_row]: 
-          cell.style = standard_cell_style        
+          cell.style = Styles.standard_cell     
         
         tests = report.clash_tests.all()
 
@@ -256,41 +238,41 @@ def create_model_info_sheets_file(bim_model):
           ws.append(['',test.date.strftime("%m/%d/%Y"), test.comments, test.clash_new, test.clash_active, test.clash_reviewed, test.clash_approved, test.clash_resolved])
           
           for cell in ws[ws.max_row]: 
-            cell.style = standard_cell_style
+            cell.style = Styles.standard_cell
         
         ws.append([])
 
       if sheet.sheet_type == 'validation':
         ws.append(['', 'Nome Report', report.name])
         for cell in ws[ws.max_row]:
-          cell.style = standard_cell_style
+          cell.style = Styles.standard_cell
           cell.font = Font(bold=True)
         ws.merge_cells(f'C{ws.max_row}:F{ws.max_row}')         
 
         ws.append(['', 'Oggetto/Specifica Report', report.description])
         for cell in ws[ws.max_row]:
-          cell.style = standard_cell_style
+          cell.style = Styles.standard_cell
         ws.merge_cells(f'C{ws.max_row}:F{ws.max_row}')
 
         for cell in ws[ws.max_row]: 
-          cell.style = standard_cell_style
+          cell.style = Styles.standard_cell
           
         tests = report.validation_tests.all()
         ws.append(['','Data', 'Commenti', 'Specifica', 'Difformità', 'Allegati'])
 
         for cell in ws[ws.max_row]: 
-          cell.style = standard_cell_style   
+          cell.style = Styles.standard_cell   
 
         for test in tests:
           ws.append(['', test.date.strftime("%m/%d/%Y"), test.comments, test.specification, test.issues, ''])
 
           for cell in ws[ws.max_row]: 
-            cell.style = standard_cell_style          
+            cell.style = Styles.standard_cell       
         
         ws.append([])
 
     ws['A10'] = 'ATTIVITA\' REPORT'
-    ws['A10'].style = report_header_style
+    ws['A10'].style = Styles.report_header
     ws.merge_cells(f'A10:A{ws.max_row}')    
 
   response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')  
